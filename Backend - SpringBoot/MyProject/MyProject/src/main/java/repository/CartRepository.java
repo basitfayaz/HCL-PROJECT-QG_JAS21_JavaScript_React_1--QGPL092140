ad2dd5e67fa5a89c6c5c1e2b02a4dc3e7e67d3af
Basit Fayaz <91362729+basitfayaz@users.noreply.github.com>
2021-12-23 12:49:04 +0530

    Delete Backend - SpringBoot/MyProject/MyProject directory

--- Backend - SpringBoot/MyProject/MyProject/src/main/java/repository/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import model.FoodCart;
-@Repository
-public interface CartRepository extends JpaRepository<FoodCart,Long>{
-
-}
